58317f342155e4eebd9dc70a53af8bd24e572901
TLB30 <thelastblade30@gmail.com>
2023-08-09 14:15:04 +0330

    build list count function and add function

---
+++ logic/index.js
@@ -0,0 +1,28 @@
+const numbers = [
+    {num:"1"},
+    {num:"2"},
+    {num:"3"}
+]
+
+function countProperties(obj){
+    let count = 0;
+    for(var prop in obj){
+        if(obj.hasOwnProperty(prop)){
+            count ++;
+        }
+    }
+    return count;
+}
+
+console.log(countProperties(numbers))
+
+function add(obj){
+    let total = 0;
+    obj.map((number) => {
+        let nextValue = number.num;
+        total += Number(nextValue); 
+    });
+    return total;
+}
+
+console.log(add(numbers))
