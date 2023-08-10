79be1b4c672db1ab52ca90ff0d5e0a7d878b0f4e
TLB30 <thelastblade30@gmail.com>
2023-08-10 17:24:34 +0330

    add back button and equal button and save content on a list

--- logic/index.js
+++ logic/index.js
@@ -1,17 +1,41 @@
-const numbers = [
-    {num:"+"},
-    {num:"20"},
-    {num:"30"}
-]
+const numbers = [];
+
 let lastNumber = "";
 let operator = "";
-const operators =["+" ,"-" , "*" , "/"];
+
+const operators =["+" ,"-" , "X" , "/"];
+
+const back = document.getElementById('back');
+const equal = document.getElementById('equal');
+const toggle = document.getElementById('toggle');
+const float = document.getElementById('float');
+const percent =document.getElementById('percent');
+const clear = document.getElementById('clear');
+
+// save each button in a list so then you can use to calculate the equation  
 
 [...document.querySelectorAll(".button")].forEach(el => el.addEventListener("click" ,() => {
         if(Number(el.textContent)){
             lastNumber += el.textContent; 
             console.log(lastNumber)
         }
+        if(operators.includes(el.textContent)){
+            numbers.push({num:lastNumber});
+            lastNumber = "";
+            numbers.push({opr:el.textContent});
+        }
+        if(el.id === 'equal' && lastNumber !== ""){
+            numbers.push({num:lastNumber});
+            lastNumber = ""
+            // also run the equation function
+        }else if(lastNumber === ""){
+            console.log("the last part of equation is an operator ")
+        }
+        if(el.id === "back"){
+            lastNumber = lastNumber.slice(0,-1);
+            console.log(lastNumber)
+        }
+        console.table(numbers);
     }
 ));
 
