82049ddb79ae496e4e21fa06fa9a23ccb7c4dd28
TLB30 <thelastblade30@gmail.com>
2023-08-16 18:07:37 +0330

    show numbers on display

--- logic/index.js
+++ logic/index.js
@@ -2,7 +2,7 @@ let numbers = [];
 
 let lastNumber = "";
 let lastObjectInList = numbers[numbers.length-1];
-const operators =['/' , 'X' , '+' ,'-'];
+const operators =['/' , '*' , '+' ,'-'];
 
 const back = document.getElementById('back');
 const equal = document.getElementById('equal');
@@ -10,67 +10,100 @@ const toggle = document.getElementById('toggle');
 const float = document.getElementById('float');
 const percent =document.getElementById('percent');
 const clear = document.getElementById('clear');
+const screen = document.getElementById("screen-section");
+let text = '';
+let result ;
+
+const dontShow = ['C' , '+/-' , '', '='];
 
 // save each button in a list so then you can use to calculate the equation  
 
 [...document.querySelectorAll(".button")].forEach(el => el.addEventListener("click" ,() => {
-        // print out numbers
-        if(Number(el.textContent)){
+    
+   
+
+    // print out list
+    if(Number(el.textContent) || el.textContent === '.' ){
+        if(text.includes('.') && el.textContent === '.'){}
+        else{
             lastNumber += el.textContent; 
-        }
-        // add object to numbers list 
-        if(operators.includes(el.textContent) && lastNumber !== ""){
-            numbers.push({num:lastNumber});
-            lastNumber = "";
-            numbers.push({opr:el.textContent});
+            console.log(lastNumber)
+            text += el.textContent;
         }
         
-        // back button
-        if(el.id === "back"){
-            lastNumber = lastNumber.slice(0,-1);
-            if(lastNumber === "" && numbers.length !== 0){
-                let lastObjectKey = Object.keys(numbers)[numbers.length-1];
-                numbers[lastObjectKey]
-                if(operators.includes(numbers[lastObjectKey].opr)){
-                    numbers.pop();
-                }else{
-                    lastNumber += numbers[lastObjectKey].num;
-                    lastNumber = lastNumber.slice(0 , -1);
-                    numbers.pop();
-                }
+    }
+    // add object to list 
+    if(operators.includes(el.textContent) && lastNumber !== ""){
+        numbers.push({num:lastNumber});
+        text += el.textContent;
+        lastNumber = "";
+        numbers.push({opr:el.textContent});
+    }
+    
+    // back button
+    if(el.id === "back"){
+        lastNumber = lastNumber.slice(0,-1);
+        text = text.slice(0,-1);
+
+        if(lastNumber === "" && numbers.length !== 0){
+            let lastObjectKey = Object.keys(numbers)[numbers.length-1];
+            numbers[lastObjectKey]
+            if(operators.includes(numbers[lastObjectKey].opr)){
+                numbers.pop();
+            }else{
+                lastNumber += numbers[lastObjectKey].num;
+                lastNumber = lastNumber.slice(0 , -1);
+                numbers.pop();
             }
         }
-        // clear button
-        if(el.id === "clear"){
-            numbers = [];
+    }
+    // clear button
+    if(el.id === "clear"){
+        numbers = [];
+        lastNumber = '';
+        text = '';
+    }
+    // toggle button
+    if(el.id === "toggle" && lastNumber !== ""){
+        if(parseFloat(lastNumber) < 0){
+            let toggleNum = Math.abs(parseFloat(lastNumber));
+            lastNumber = toggleNum.toString();
+        }else if(parseFloat(lastNumber) > 0){
+            lastNumber = (-parseFloat(lastNumber)).toString();
         }
-        // toggle button
-        if(el.id === "toggle" && lastNumber !== ""){
-            if(parseInt(lastNumber) <0){
-               let toggleNum = Math.abs(parseInt(lastNumber));
-               lastNumber = toggleNum.toString();
-            }else if(parseInt(lastNumber) > 0){
-                lastNumber = (-parseInt(lastNumber)).toString();
-            }
-        }
-        // equal button
-        if(el.id === 'equal' && lastNumber !== "" && numbers.some(()=>{
-            return numbers.map((obj)=> obj.opr);
-        }) && operators.includes(numbers[numbers.length-1].opr)){
-            // equation function(needed)
-            numbers.push({num:lastNumber});
-                const result = calculate(numbers);
-                console.log(result);
-            lastNumber = ""
-            console.log(lastNumber);
+        text = lastNumber;
+    }
+    // equal button
+    if(el.id === 'equal' && lastNumber !== "" && numbers.some(()=>{
+        return numbers.map((obj)=> obj.opr);
+    }) && operators.includes(numbers[numbers.length-1].opr)){
+        // equation function(needed)
+        numbers.push({num:lastNumber});
+
+            result = calculate(numbers);
+            result = Number(parseFloat(result.toFixed(5)));
+
             console.table(numbers);
-        }else if (el.id === "equal" && lastNumber === "" && numbers === []){
-            console.log('enter a number')
-            if(numbers !== [] && lastObjectInList.hasOwnProperty('opr'))
-            console.log("doing sth wrong");
-        }
 
+            numbers = [];
+            text = String(result);
+            lastNumber = '';
+            lastNumber += String(result);
 
+        console.log(lastNumber);
+        
+    }else if (el.id === "equal" && lastNumber === "" && numbers === []){
+        console.log('enter a number')
+        if(numbers !== [] && lastObjectInList.hasOwnProperty('opr'))
+        console.log("doing sth wrong");
+    }
+
+    screen.innerHTML = `<div id="screen-section" class="flex overflow-hidden flex-row items-center justify-end pr-5 w-[92%] h-[200px] bg-yellow-950 mt-4 rounded-xl">
+    <span class="font-sans text-6xl text-white"> ${text}</span>
+</div>`
+
+    console.log(typeof(text))
+    console.table(numbers)
         
 
     }
@@ -78,7 +111,6 @@ const clear = document.getElementById('clear');
 
 
 
-const screen = document.getElementById("screen-section");
 
 function countProperties(obj){
     let count = 0;
@@ -91,82 +123,51 @@ function countProperties(obj){
 }
 
 
-function add(obj , index){
-    let total = Number(obj[index-1].num);
-    let nextValue = Number(obj[index+1].num);
-    total += nextValue;
-    return total;
-}
-
-function subtract(obj , index){
-    let total = Number(obj[index-1].num);
-    let nextValue = Number(obj[index+1].num);
-    total -= nextValue;
-    return total;
-}
-
-function multiply(obj , index){
-    let total = Number(obj[index-1].num);
-    let nextValue = Number(obj[index+1].num);
-    total *= nextValue;
-    return total;
-}
-
-function divide(obj , index){
-    let total = Number(obj[index-1].num);
-    let nextValue = Number(obj[index+1].num);
-    total /= nextValue;
-    return total;
-}
-    // got help from chat gpt since i couldn't fix the operator order 
+    // i rewrote the function and learned it's fundamentals
 function calculate(equationList){
-    
-        const precedence = {'+': 1, '-': 1, 'X': 2, '/': 2};
-        const outputQueue = [];
-        const operatorStack = [];
-    
-        for (const token of equationList) {
-            console.log(token)
-            if (token.num) {
-                outputQueue.push(parseFloat(token.num));
-            } else if (token.opr) {
+        const precedence = {'+':1, '-':1 , '*':2 , '/':2};
+        const queueList =[];
+        const stackList = [];
+
+        for (const token of equationList){
+            if(token.num){
+                queueList.push(parseFloat(token.num));
+            } else if(token.opr){
                 const operator = token.opr;
-                console.log(operator)
-                while (
-                    operatorStack.length > 0 &&
-                    precedence[operatorStack[operatorStack.length - 1]] >= precedence[operator]
-                ) {
-                    outputQueue.push(operatorStack.pop());
-                    console.log(outputQueue);
-                    console.log(operatorStack)
+                while(
+                    stackList.length > 0 &&
+                    precedence[stackList[stackList.length-1]] >= precedence[operator]
+                ){
+                    queueList.push(stackList.pop());
+                    console.log(queueList);
                 }
-                operatorStack.push(operator);
+                stackList.push(operator);
             }
         }
-    
-        while (operatorStack.length > 0) {
-            outputQueue.push(operatorStack.pop());
+
+        while(stackList.length > 0){
+            queueList.push(stackList.pop());
         }
-    
         const resultStack = [];
-        for (const token of outputQueue) {
-            if (typeof token === 'number') {
+        for(const token of queueList){
+            if(typeof token === 'number'){
                 resultStack.push(token);
             } else {
                 const b = resultStack.pop();
                 const a = resultStack.pop();
-                if (token === '+') {
-                    resultStack.push(a + b);
-                } else if (token === '-') {
-                    resultStack.push(a - b);
-                } else if (token === 'X') {
-                    resultStack.push(a * b);
-                } else if (token === '/') {
-                    resultStack.push(a / b);
-                }
+                if(token === '-'){
+                    resultStack.push(a-b);
+                } else if(token === '+'){
+                    resultStack.push(a+b);
+                } 
+                else if(token === '/'){
+                    resultStack.push(a/b);
+                } 
+                else if(token === '*'){
+                    resultStack.push(a*b);
+                } 
             }
         }
-    
         return resultStack[0];
     }
     
